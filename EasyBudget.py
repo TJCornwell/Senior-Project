d214c80f38b70fcb67260097c9c225d38d4b2abc
Tim Cornwell <80080215+TJCornwell@users.noreply.github.com>
2024-02-16 01:40:47 -0600

    update css

--- EasyBudget.py
+++ EasyBudget.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('HomePage.html')
+    return render_template('ProfilePage.html')
     
 
 if __name__==('__main__'):
