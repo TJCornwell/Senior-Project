f3321a99ea90f3c0d42edcbfef7fdd320aee395f
cody1936 <116526689+cody1936@users.noreply.github.com>
2024-02-26 18:20:24 -0600

    fixed css links. Deleted reset password link. Added routes links.

--- EasyBudget.py
+++ EasyBudget.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('HomePage.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
     
 
 if __name__==('__main__'):
